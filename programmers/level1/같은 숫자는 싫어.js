31a9aae35e6cd0fc3c4ef266af97750312359396
jeonchaei <ahswn1031@gmail.com>
2021-08-28 17:29:51 +0900

    2021-08-28

--- programmers/level1/같은 숫자는 싫어.js
+++ programmers/level1/같은 숫자는 싫어.js
@@ -1,8 +1,9 @@
 function solution(arr)
 {
-    return arr.filter((element,index)=>{
-        if(element===arr[index+1]) {
-            return false; 
-        }else return true;
-    });
+    // return arr.filter((element,index)=>{
+    //     if(element===arr[index+1]) {
+    //         return false; 
+    //     }else return true;
+    // });
+    return arr.filter((element,index)=>element!=arr[index+1]);
 }
