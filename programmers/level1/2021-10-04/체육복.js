41ba039f9154be67abba0d267137f0ab3c5842e0
jeonchaei <ahswn1031@gmail.com>
2021-10-04 13:32:26 +0900

    2021-10-04

---
+++ programmers/level1/2021-10-04/체육복.js
@@ -0,0 +1,23 @@
+function solution(n, lost, reserve) {
+    var answer = 0;
+    lost.sort();
+    reserve.sort();
+    for(let j=1;j<=n;j++){
+        if(lost.includes(j)&&reserve.includes(j)){
+            reserve.splice(reserve.indexOf(j),1);
+            lost.splice(lost.indexOf(j),1);
+        }
+    }
+    console.log(lost);
+    console.log(reserve);
+    lost.forEach(e=>{
+        for(let i=0;i<reserve.length;i++){
+            if(e==reserve[i]-1||e==reserve[i]+1){
+                reserve[i]=-1;
+                answer++;
+                break;
+            }
+        }
+    })
+    return n-lost.length+answer;
+}
